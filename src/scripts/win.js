a71def93cb5afcd40c072dd4c39fca42e663c58e
adam allalou <aabou9@gmail.com>
2020-09-13 17:57:38 +0100

    add win.js

--- src/scripts/win.js
+++ src/scripts/win.js
@@ -1,3 +1,13 @@
+const winComb = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2],
+];
 const win = (array) => {
   let hasWon = false;
   winComb.forEach((item) => {
@@ -13,3 +23,4 @@ const win = (array) => {
   });
   return hasWon;
 };
+export {win};
