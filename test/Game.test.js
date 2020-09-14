3c337e6c894e9b9677eb51cc9306ca270e2939c8
adam allalou <aabou9@gmail.com>
2020-09-14 12:21:28 +0100

    fix game test issue

--- test/Game.test.js
+++ test/Game.test.js
@@ -7,8 +7,11 @@ describe('should check is a valide move', () => {
     expect(gameLogic.board).toStrictEqual([null, null, null, null, null, null, null, null, null]);
   });
   
+});
+
+describe('should check the move is added to board array', () => {
+  let gameLogic = GameBoard([null, null, null, null, null, null, null, null, null]);
   gameLogic.game(5, gameLogic.board);
-  
   it('should add X to index 5', () => {
     expect(gameLogic.board).toStrictEqual([null, null, null, null, null, 'X', null, null, null]);
   });
